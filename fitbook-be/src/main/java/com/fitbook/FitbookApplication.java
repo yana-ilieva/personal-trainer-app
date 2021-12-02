eb41ac79aae03a6188868e7efc1bedc6c063fbfa
Yana Ilieva <yanailieva199@gmail.com>
2021-12-02 22:03:18 +0200

    initialize app starting point

---
+++ fitbook-be/src/main/java/com/fitbook/FitbookApplication.java
@@ -0,0 +1,11 @@
+package com.fitbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitbookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FitbookApplication.class, args);
+    }
+}
