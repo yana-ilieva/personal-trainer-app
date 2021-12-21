785590cd7406dfe36f4bc98104c0c593e4586d51
Mariyan <mariyan@enigmadigital.co>
2021-12-21 19:48:15 +0200

    registration layout added

--- fitbook-fe/src/App.vue
+++ fitbook-fe/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="nav">
+  <registration></registration>
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <router-view /> -->
 </template>
+
+<script>
+import Registration from './components/Registration.vue';
+export default {
+  components: { Registration },
+};
+</script>
 
 <style>
 #app {
--- fitbook-fe/src/components/Registration.vue
+++ fitbook-fe/src/components/Registration.vue
@@ -1,5 +1,39 @@
 <template>
-  
+  <div class="w-11/12 h-11/12">
+    <div class="flex">
+      <div class="h-10/12 w-2/5 bg-mint"></div>
+      <div class="h-10/12 p-10">
+        <div class="flex flex-col items-center justify-center">
+          <!-- toggle button -->
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+          <!-- main elements -->
+          <div>
+            <h1 class="text-center mb-16">Register as Trainer</h1>
+            <form class="flex">
+              <div>
+                <input type="text" placeholder="First Name" />
+                <input type="text" placeholder="Last Name" />
+                <input type="text" placeholder="Password" />
+                <input type="text" placeholder="Confirm Password" />
+                <input type="radio" id="male" name="sex" value="male" checked />
+                <input type="radio" id="female" name="sex" value="female" />
+              </div>
+              <div>
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Address" />
+                <input type="text" placeholder="Phone Number" />
+                <input type="submit" placeholder="Register Now" />
+                <a href="#">or Login</a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
