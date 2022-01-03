59fde81b51659fd701fc2d43432a928a973955f1
Mariyan <mariyan@enigmadigital.co>
2022-01-03 13:47:53 +0200

    styling registration page

--- fitbook-fe/src/components/Registration.vue
+++ fitbook-fe/src/components/Registration.vue
@@ -1,34 +1,76 @@
 <template>
-  <div class="w-11/12 h-11/12">
-    <div class="flex">
-      <div class="h-10/12 w-2/5 bg-mint"></div>
-      <div class="h-10/12 p-10">
-        <div class="flex flex-col items-center justify-center">
-          <!-- toggle button -->
-          <div>
-            <div></div>
-            <div></div>
-          </div>
-          <!-- main elements -->
-          <div>
-            <h1 class="text-center mb-16">Register as Trainer</h1>
-            <form class="flex">
-              <div>
-                <input type="text" placeholder="First Name" />
-                <input type="text" placeholder="Last Name" />
-                <input type="text" placeholder="Password" />
-                <input type="text" placeholder="Confirm Password" />
-                <input type="radio" id="male" name="sex" value="male" checked />
-                <input type="radio" id="female" name="sex" value="female" />
+  <div class="w-full p-12">
+    <div class="flex justify-center items-center">
+      <div class="flex w-full">
+        <div class="h-11/12 w-2/5 rounded-md bg-mint"></div>
+        <div class="h-10/12 w-full ml-10">
+          <div
+            class="p-20 border border-darkmint rounded-md flex flex-col items-center justify-center"
+          >
+            <!-- toggle button -->
+            <div class="mb-8 flex self-end">
+              <div
+                @click="isTrainer = true"
+                class="border border-darkmint px-4 py-1 cursor-pointer rounded-tl-md rounded-bl-md"
+                :class="isTrainer ? 'text-white bg-darkmint' : ''"
+              >
+                Trainer
               </div>
-              <div>
-                <input type="text" placeholder="Email" />
-                <input type="text" placeholder="Address" />
-                <input type="text" placeholder="Phone Number" />
-                <input type="submit" placeholder="Register Now" />
-                <a href="#">or Login</a>
+              <div
+                @click="isTrainer = false"
+                class="border border-darkmint cursor-pointer px-4 py-1 rounded-tr-md rounded-br-md"
+                :class="isTrainer ? '' : 'text-white bg-darkmint'"
+              >
+                Client
               </div>
-            </form>
+            </div>
+            <!-- main elements -->
+            <div class="p-10">
+              <h1 class="text-center mb-10 text-3xl">
+                Register as {{ isTrainer ? 'Trainer' : 'Client' }}
+              </h1>
+              <form class="flex">
+                <div class="flex flex-col">
+                  <input class="mb-8" type="text" placeholder="First Name" />
+                  <input class="mb-8" type="text" placeholder="Last Name" />
+                  <input class="mb-8" type="text" placeholder="Password" />
+                  <input
+                    class="mb-8"
+                    type="text"
+                    placeholder="Confirm Password"
+                  />
+                  <div class="flex items-center mb-4">
+                    <input
+                      type="radio"
+                      id="male"
+                      name="sex"
+                      value="male"
+                      checked
+                    />
+                    <label class="ml-4" for="male">Male</label>
+                  </div>
+                  <div class="flex items-center">
+                    <input type="radio" id="female" name="sex" value="female" />
+                    <label class="ml-4" for="female">Female</label>
+                  </div>
+                </div>
+                <div class="flex flex-col ml-10 items-center">
+                  <input class="mb-8" type="text" placeholder="Email" />
+                  <input class="mb-8" type="text" placeholder="Address" />
+                  <input class="mb-8" type="text" placeholder="Phone Number" />
+                  <input
+                    class="mb-4 px-10 py-4 bg-mint hover:bg-darkmint cursor-pointer transition-all rounded-md text-white"
+                    type="submit"
+                    value="Register Now"
+                  />
+                  <a
+                    class="bg-lbeige hover:bg-beige cursor-pointer transition-all rounded-md w-max px-4 py-1"
+                    href="#"
+                    >Login</a
+                  >
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -40,7 +82,7 @@
 export default {
   data() {
     return {
-      test: 'test',
+      isTrainer: true,
     };
   },
 };
