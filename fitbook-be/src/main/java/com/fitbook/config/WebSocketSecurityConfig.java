408d4437744427102b4132074973e6f7459366a5
Yana Ilieva <yanailieva199@gmail.com>
2022-01-08 13:17:39 +0200

    test ws security

--- fitbook-be/src/main/java/com/fitbook/config/WebSocketSecurityConfig.java
+++ fitbook-be/src/main/java/com/fitbook/config/WebSocketSecurityConfig.java
@@ -1,10 +1,16 @@
 package com.fitbook.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
-//@Configuration
+@Configuration
 public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages.simpDestMatchers("/secured/**").authenticated().anyMessage().authenticated();
+    }
 
     @Override
     protected boolean sameOriginDisabled() {
